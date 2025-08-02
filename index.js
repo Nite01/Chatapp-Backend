5c299187d5c392703c06f633c028b55c25279a9c
Nitesh Pathak <113054500+Nite01@users.noreply.github.com>
2025-08-02 23:56:22 +0530

    new
    new

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
-// import path from "path";
+import path from "path";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
-// const __dirname = path.resolve();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middlewares
 app.use(express.json());
@@ -31,13 +31,13 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-// //   app.get("*", (req, res) => {
-// //     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-// //   });
-// // }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server running on PORT:", PORT);
